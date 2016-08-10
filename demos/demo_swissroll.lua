85301df6dfb6d3e89a9dd0becb188393be1f83e3
Alfredo Canziani <alfredo.canziani@gmail.com>
2016-08-10 16:28:02 -0400

    Update demos and documentation
    Remove `gfx.js` dependencies and use default packages `image` and `gnuplot`
    Fix bad alignment for demos code
    Add dependencies installation instructions to documentation
     - Without `libatlas3-base`, `bhtsne_linux` won't find `libcblas.so.3`

--- demos/demo_swissroll.lua
+++ demos/demo_swissroll.lua
@@ -1,18 +1,22 @@
 -- This is the Swissroll test...
-require 'torch'
-mani = require 'manifold'
-require 'gfx.js'
+local mani = require 'manifold'
+local g = require 'gnuplot'
 
+local function newFig(t) g.figure(); g.grid(true); g.title(t) end
 local N = 2000
 local K = 12
 local sigma = 7
 local d = 2
 
-local tt = torch.mul(torch.sqrt(torch.rand(1, N)), 4*3.1416)
+local tt = torch.mul(torch.sqrt(torch.rand(1, N)), 4 * math.pi)
 local X = torch.DoubleTensor(3, N)
 X[1] = torch.cmul(torch.add(tt, 0.1), torch.cos(tt))
 X[2] = torch.cmul(torch.add(tt, 0.1), torch.sin(tt))
-X[3] = torch.mul(torch.rand(1, N), 8*3.1416)
+X[3] = torch.mul(torch.rand(1, N), 8 * math.pi)
+
+newFig('Original 3D')
+g.scatter3(X[1], X[2], X[3])
+
 X = X:t()
 
 print('random embedding...')
@@ -20,7 +24,8 @@ Y = mani.embedding.random(X, {
    dim = 2,
 })
 
-gfx.chart({values=Y, key='Random'}, {chart='scatter', width=1024, height=800})
+newFig('Random')
+g.plot(Y, '+')
 
 print('LLE embedding...')
 Y = mani.embedding.lle(X, {
@@ -29,7 +34,8 @@ Y = mani.embedding.lle(X, {
    tol = 1e-3
 })
 
-gfx.chart({values=Y, key='LLE'}, {chart='scatter', width=1024, height=800})
+newFig('LLE')
+g.plot(Y, '+')
 
 print('Laplacian Eigenmaps embedding...')
 local K = 60
@@ -37,4 +43,5 @@ Y = mani.embedding.laplacian_eigenmaps(X, {
    dim = 2,neighbors = K,sigma = sigma,normalized = false
 })
 
-gfx.chart({values=Y, key='Laplacian Eigenmaps'}, {chart='scatter', width=1024, height=800})
+newFig('Laplacian Eigenmaps')
+g.plot(Y, '+')
--- demos/demo_tsne.lua
+++ demos/demo_tsne.lua
@@ -1,86 +1,82 @@
-
 local manifold = require 'manifold'
+require 'gnuplot'
+require 'image'
 
 -- function to show an MNIST 2D group scatter plot:
-local function show_scatter_plot(mapped_x, labels)
+local function show_scatter_plot(method, mapped_x, labels)
 
-  -- count label sizes:
-  local K = 10
-  local cnts = torch.zeros(K)
-  for n = 1,labels:nElement() do
-    cnts[labels[n] + 1] = cnts[labels[n] + 1] + 1
-  end
+   -- count label sizes:
+   local K = 10
+   local cnts = torch.zeros(K)
+   for n = 1,labels:nElement() do
+      cnts[labels[n] + 1] = cnts[labels[n] + 1] + 1
+   end
 
-  -- separate mapped data per label:
-  mapped_data = {}
-  for k = 1,K do
-    mapped_data[k] = { key = 'Digit ' .. k - 1, values = torch.Tensor(cnts[k], opts.ndims) }
-  end
-  local offset = torch.Tensor(K):fill(1)
-  for n = 1,labels:nElement() do
-    mapped_data[labels[n] + 1].values[offset[labels[n] + 1]]:copy(mapped_x[n])
-    offset[labels[n] + 1] = offset[labels[n] + 1] + 1
-  end
+   -- separate mapped data per label:
+   mapped_data = {}
+   for k = 1,K do
+      mapped_data[k] = {'Digit ' .. k-1, torch.Tensor(cnts[k], opts.ndims), '+'}
+   end
+   local offset = torch.Tensor(K):fill(1)
+   for n = 1,labels:nElement() do
+      mapped_data[labels[n] + 1][2][offset[labels[n] + 1]]:copy(mapped_x[n])
+      offset[labels[n] + 1] = offset[labels[n] + 1] + 1
+   end
 
-  -- show results in scatter plot:
-  local gfx = require 'gfx.js'
-  gfx.chart(mapped_data, {
-     chart = 'scatter',
-     width = 600,
-     height = 600,
-  })
+   -- show results in scatter plot:
+   gnuplot.figure(); gnuplot.grid(true); gnuplot.title(method)
+   gnuplot.plot(mapped_data)
 end
 
 
 -- show map with original digits:
-local function show_map(mapped_data, X)
-  
-  -- draw map with original digits:
-  local im_size = 2048
-  local background = 0
-  local background_removal = true
-  local map_im = manifold.draw_image_map(mapped_data, X:resize(X:size(1), 1, 28, 28), im_size, background, background_removal)
-  
-  -- plot results:
-  local gfx = require 'gfx.js'
-  gfx.image(map_im)
+local function show_map(method, mapped_data, X)
+
+   -- draw map with original digits:
+   local im_size = 2048
+   local background = 0
+   local background_removal = true
+   map_im = manifold.draw_image_map(mapped_data, X:resize(X:size(1), 1, 28, 28), im_size, background, background_removal)
+
+   -- plot results:
+   image.display{image=map_im, legend=method, zoom=0.5}
+   image.savePNG(method .. '.png', map_im)
 end
 
 
 -- function that performs demo of t-SNE code on MNIST:
 local function demo_tsne()
 
-  -- amount of data to use for test:
-  local N = 5000
+   -- amount of data to use for test:
+   local N = 5000
 
-  -- load subset of MNIST test data:
-  local mnist = require 'mnist'
-  local testset = mnist.testdataset()
-  testset.size  = N
-  testset.data  = testset.data:narrow(1, 1, N)
-  testset.label = testset.label:narrow(1, 1, N)
-  local x = torch.DoubleTensor(testset.data:size()):copy(testset.data)
-  x:resize(x:size(1), x:size(2) * x:size(3))
-  local labels = testset.label
+   -- load subset of MNIST test data:
+   local mnist = require 'mnist'
+   local testset = mnist.testdataset()
+   testset.size  = N
+   testset.data  = testset.data:narrow(1, 1, N)
+   testset.label = testset.label:narrow(1, 1, N)
+   local x = torch.DoubleTensor(testset.data:size()):copy(testset.data)
+   x:resize(x:size(1), x:size(2) * x:size(3))
+   local labels = testset.label
 
-  -- run t-SNE:
-  local timer = torch.Timer()
-  opts = {ndims = 2, perplexity = 30, pca = 50, use_bh = false}
-  mapped_x1 = manifold.embedding.tsne(x, opts)
-  print('Successfully performed t-SNE in ' .. timer:time().real .. ' seconds.')
-  show_scatter_plot(mapped_x1, labels)
-  show_map(mapped_x1, x:clone())
+   -- run t-SNE:
+   local timer = torch.Timer()
+   opts = {ndims = 2, perplexity = 30, pca = 50, use_bh = false}
+   mapped_x1 = manifold.embedding.tsne(x, opts)
+   print('Successfully performed t-SNE in ' .. timer:time().real .. ' seconds.')
+   show_scatter_plot('t-SNE', mapped_x1, labels)
+   show_map('t-SNE', mapped_x1, x:clone())
 
-  -- run Barnes-Hut t-SNE:
-  opts = {ndims = 2, perplexity = 30, pca = 50, use_bh = true, theta = 0.5}
-  timer:reset()
-  mapped_x2 = manifold.embedding.tsne(x, opts)
-  print('Successfully performed Barnes Hut t-SNE in ' .. timer:time().real .. ' seconds.')
-  show_scatter_plot(mapped_x2, labels)
-  show_map(mapped_x2, x:clone())
+   -- run Barnes-Hut t-SNE:
+   opts = {ndims = 2, perplexity = 30, pca = 50, use_bh = true, theta = 0.5}
+   timer:reset()
+   mapped_x2 = manifold.embedding.tsne(x, opts)
+   print('Successfully performed Barnes Hut t-SNE in ' .. timer:time().real .. ' seconds.')
+   show_scatter_plot('Barnes-Hut t-SNE', mapped_x2, labels)
+   show_map('Barnes-Hut t-SNE', mapped_x2, x:clone())
 end
 
 
 -- run the demo:
 demo_tsne()
-
